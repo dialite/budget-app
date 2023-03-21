3579805cc8dff83169186964d839d9f2bedae0b6
dialite <raymondolisa01@gmail.com>
2023-03-21 10:43:08 +0100

    Add relationships to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :group_expenses, class_name: 'GroupExpense', foreign_key: 'group_id', dependent: :destroy
 end
