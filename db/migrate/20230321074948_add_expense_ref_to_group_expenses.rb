2843ca61b587c60b02f1d8b186778f27f6cd094f
dialite <raymondolisa01@gmail.com>
2023-03-21 08:51:03 +0100

    Add expense ref to group expenses table

---
+++ db/migrate/20230321074948_add_expense_ref_to_group_expenses.rb
@@ -0,0 +1,4 @@
+class AddExpenseRefToGroupExpenses < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :group_expenses, :expenses, null: false, foreign_key: { to_table: 'expenses' }
+end
