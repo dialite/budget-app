b4e2a58b10c8c20e45a47a1e979192a49bd5a4d0
dialite <raymondolisa01@gmail.com>
2023-03-21 10:59:03 +0100

    Crrate validation for user, group and expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :group_expenses, class_name: 'GroupExpense', foreign_key: 'expense_id', dependent: :destroy
+
+  validates :name, :amount, presence: true
 end
