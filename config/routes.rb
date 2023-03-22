f25194af680f1d5836ae352c7f3de87da9348745
dialite <raymondolisa01@gmail.com>
2023-03-22 17:55:15 +0100

    Modify add group and expense refs in migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do  devise_for :users
 
-  resources :home, :only [:index]
+  resources :home, only: [:index]
   resources :group_expenses
   resources :expenses
   resources :groups
