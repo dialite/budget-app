5a1f891773d1bd9859635f498df99eb5faf816cb
dialite <raymondolisa01@gmail.com>
2023-03-23 17:24:07 +0100

    Update relationship of groups and expenses in model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :group_expenses, class_name: 'GroupExpense', foreign_key: 'expense_id', dependent: :destroy
+  has_many :groups, through: :group_expenses
 
   validates :name, :amount, presence: true
   validates :amount, presence: true, numericality: { only_float: true, greater_than_or_equal_to: 0 }
