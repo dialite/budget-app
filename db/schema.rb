78faa201552f9f3b14e4b5b8cef1efac601cad98
dialite <raymondolisa01@gmail.com>
2023-03-23 12:53:38 +0100

    Add comfirmables to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_172258) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_112318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_172258) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "role"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
