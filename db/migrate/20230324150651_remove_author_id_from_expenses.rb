f8300a415a62bbb6e7ba8c4a1ece59f4a55df627
dialite <raymondolisa01@gmail.com>
2023-03-24 16:33:30 +0100

    Reslove table conflict and fix errors from expenses and groups view

---
+++ db/migrate/20230324150651_remove_author_id_from_expenses.rb
@@ -0,0 +1,4 @@
+class RemoveAuthorIdFromExpenses < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_034711) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_151030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_034711) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
+    t.bigint "user_id", null: false
     t.index ["author_id"], name: "index_expenses_on_author_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "group_expenses", force: :cascade do |t|
@@ -89,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_034711) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "expenses", "users"
   add_foreign_key "expenses", "users", column: "author_id"
   add_foreign_key "group_expenses", "expenses"
   add_foreign_key "group_expenses", "groups"
