1873d8762f403554c3cea23ee55b65c4ee619ee6
dialite <raymondolisa01@gmail.com>
2023-03-24 15:00:21 +0100

    Add has one attached icon to expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,6 +3,8 @@ class Expense < ApplicationRecord
   has_many :group_expenses
   has_many :groups, through: :group_expenses
 
+  has_one_attached :icon
+  
   validates :name, :amount, presence: true
   validates :amount, presence: true, numericality: { only_float: true, greater_than_or_equal_to: 0 }
 end
