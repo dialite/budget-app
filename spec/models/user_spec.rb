81b7822859f1f189f0816294dd9b32ebb92eb9a1
dialite <raymondolisa01@gmail.com>
2023-03-26 04:24:43 +0100

    Fix rubocop errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  subject {
-         described_class.new(name: 'Raymond', password: "newlight", email: "contentcreator@fun.com", id: 1)
-  }
+RSpec.describe User, type: :model do
+  subject do
+    described_class.new(name: 'Raymond', password: 'newlight', email: 'contentcreator@fun.com', id: 1)
+  end
 
-  describe "Validations" do
-    it "is valid with valid attributes" do
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
 
-    it "is not valid without a password" do
+    it 'is not valid without a password' do
       subject.password = nil
       expect(subject).to_not be_valid
     end
 
-    it "is not valid without an email" do
+    it 'is not valid without an email' do
       subject.email = nil
       expect(subject).to_not be_valid
     end
 
-    it "is not valid without an name" do
+    it 'is not valid without an name' do
       subject.name = nil
       expect(subject).to_not be_valid
     end
